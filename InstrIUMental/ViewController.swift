d82bf772fcc47de391c4f94e0c0ad1eacaf1630b
Giacomo1995 <giacomo.meloni1995@gmail.com>
2019-02-06 17:45:45 +0100

    Prova Giacomo

--- InstrIUMental/ViewController.swift
+++ InstrIUMental/ViewController.swift
@@ -9,10 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-    var roms: String!
-    var prova: String!
-    var x: String!
-    var andrea: String!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
