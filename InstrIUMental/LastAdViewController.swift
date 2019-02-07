a1604aed3ddb1df1f283af27189ccb567849eca2
marcoortu96 <ortumarco@hotmail.it>
2019-02-07 20:15:16 +0100

    sistemati warning e aggiunta view Ultimi Annunci

---
+++ InstrIUMental/LastAdViewController.swift
@@ -0,0 +1,31 @@
+//
+//  LastAdViewController.swift
+//  InstrIUMental
+//
+//  Created by Marco Ortu on 07/02/2019.
+//  Copyright © 2019 Sora. All rights reserved.
+//
+
+import UIKit
+
+class LastAdViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    @IBOutlet weak var tableView: UITableView?
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "AdsTableViewCell")
+        return cell!
+    }
+    
+
+}
