ad544ebd18ca67cc9d8bf7ffc99c9d7a635d293f
andreaRe97 <a.re@studenti.unica.it>
2019-02-13 17:05:22 +0100

    modificato AdDetail

--- InstrIUMental/Controller/LastAdViewController.swift
+++ InstrIUMental/Controller/LastAdViewController.swift
@@ -92,6 +92,8 @@ class LastAdViewController: UIViewController, UITableViewDataSource, UITableView
         
         userLogged.text = UserFactory.getLoggedUser(usrs: usrs.getUsers())?.getName()
         userImage.image = UIImage(named: UserFactory.getLoggedUser(usrs: usrs.getUsers()).getURLimage())
+        userImage.contentMode = .scaleAspectFit
+        userImage.backgroundColor = UIColor.white
         userImage.setRounded()
         
     }
