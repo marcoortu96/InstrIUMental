5fd76312588c73a65c74a38f55292d4c95dd766d
Giacomo1995 <giacomo.meloni1995@gmail.com>
2019-02-13 22:17:40 +0100

    Implementato inserimento annuncio (da migliorare)

---
+++ InstrIUMental/Controller/InsertAdController.swift
@@ -0,0 +1,21 @@
+//
+//  InsertAdController.swift
+//  InstrIUMental
+//
+//  Created by Giacomo Meloni on 13/02/2019.
+//  Copyright © 2019 Sora. All rights reserved.
+//
+
+import UIKit
+
+class InsertAdController: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
