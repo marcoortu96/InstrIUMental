8b970fb1192bdcc4cac5fe71d09f1f753d6c066a
Giacomo1995 <giacomo.meloni1995@gmail.com>
2019-02-13 20:52:06 +0100

    Implementata InsertAdViewController e creati i collegamenti in aggiungi annuncio

---
+++ InstrIUMental/Controller/InsertAdViewController.swift
@@ -0,0 +1,13 @@
+//
+//  InsertAdViewController.swift
+//  InstrIUMental
+//
+//  Created by Giacomo Meloni on 13/02/2019.
+//  Copyright © 2019 Sora. All rights reserved.
+//
+
+import UIKit
+
+class InsertAdViewController: NSObject {
+
+}
