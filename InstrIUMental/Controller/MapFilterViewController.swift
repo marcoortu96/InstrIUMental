7e13b7000b584c3a494537eb456bb5f21920ad67
Emanuele Spano <emanuelespano88@gmail.com>
2019-02-18 12:42:34 +0100

    creata nuova view per filtri mappa

---
+++ InstrIUMental/Controller/MapFilterViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MapFilterViewController.swift
+//  InstrIUMental
+//
+//  Created by batcave on 18/02/2019.
+//  Copyright © 2019 Sora. All rights reserved.
+//
+
+import UIKit
+
+class MapFilterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
